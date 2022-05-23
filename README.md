4d95466a892b98a564f6ed415ef4a6b7d5db29ad
junyi <junyi.fang@tuya.com>
2022-05-24 00:04:33 +0800

    feat: swagger

---
+++ README.md
@@ -0,0 +1,9 @@
+## 启动服务
+```
+mvn spring-boot:run
+```
+
+## 安装依赖
+```
+mvn package
+```
--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
@@ -9,9 +8,19 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @RestController
 @SpringBootApplication
+@EnableSwagger2
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
@@ -31,4 +40,26 @@ public class Main {
             }
         };
     }
+
+    @Bean
+    public Docket createRestApi() {
+           return new Docket(DocumentationType.SWAGGER_2)
+                   .apiInfo(apiInfo())
+                   .select()
+                //    .apis(RequestHandlerSelectors.basePackage("com.blog.tutorial.controller"))
+                   .paths(PathSelectors.any())
+                   .build();
+       }
+       private ApiInfo apiInfo() {
+           return new ApiInfoBuilder()
+                   .title("swagger-bootstrap-ui RESTful APIs")
+                   .description("swagger-bootstrap-ui")
+                   .termsOfServiceUrl("http://localhost:7001/")
+                   .license("http://localhost:7001/")
+                   .licenseUrl("http://localhost:7001/")
+                //    .contact("developer@mail.com")
+                   .version("1.0")
+                   .build();
+       }
+
 }
